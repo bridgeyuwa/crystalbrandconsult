76bba0c84494c45768da8ac062ea29bebb8ce3f0
bridgeyuwa <bridgeyuwa@gmail.com>
2025-06-30 13:17:58 +0100

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,9 @@ export const metadata = {
     template: '%s - Crystal Brand Educational Consult',
     default: 'Crystal Brand Educational Consult',
   },
+  icons: {
+    icon: '/favicon.ico?v=2',
+  },
   manifest: '/site.webmanifest',
 }
 
